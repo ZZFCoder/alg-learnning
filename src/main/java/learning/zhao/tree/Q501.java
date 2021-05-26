751a50a121db99b51d010f77cdcca3fb2f6e192b
zhaozhifeng10 <444122886@qq.com>
2021-05-26 09:58:21 +0800

    tag:tree Q:501 == 使用包装类判空 代替一下

--- src/main/java/learning/zhao/tree/Q501.java
+++ src/main/java/learning/zhao/tree/Q501.java
@@ -83,6 +83,101 @@ public class Q501 {
             find(root.right);
         }
     }
+
+    //给定一个有相同值的二叉搜索树（BST），找出 BST 中的所有众数（出现频率最高的元素）。
+//
+// 假定 BST 有如下定义：
+//
+//
+// 结点左子树中所含结点的值小于等于当前结点的值
+// 结点右子树中所含结点的值大于等于当前结点的值
+// 左子树和右子树都是二叉搜索树
+//
+//
+// 例如：
+//给定 BST [1,null,2,2],
+//
+//    1
+//    \
+//     2
+//    /
+//   2
+//
+//
+// 返回[2].
+//
+// 提示：如果众数超过1个，不需考虑输出顺序
+//
+// 进阶：你可以不使用额外的空间吗？（假设由递归产生的隐式调用栈的开销不被计算在内）
+// Related Topics 树
+// 👍 306 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    static class Solution2 {
+        List<Integer> ret = new ArrayList<>();
+        int pre = -1;
+        int max = 1;
+        int count = 0;
+        public int[] findMode(TreeNode root) {
+            find1(root);
+
+            int[] retArr = new int[ret.size()];
+            for (int i = 0; i < ret.size(); i++) {
+                retArr[i] = ret.get(i);
+            }
+            return retArr;
+        }
+
+        void find1(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+
+            find1(root.left);
+            if (root.val != pre && pre != -1) {
+                count = 1;
+                pre = root.val;
+                if (count == max) {
+                    ret.add(root.val);
+                }
+            }
+            else if (pre == -1 || root.val == pre) {
+                count ++;
+                if (count == max) {
+                    ret.add(root.val);
+                }
+
+                if (count > max) {
+                    ret.clear();
+                    ret.add(root.val);
+                    max = count;
+                }
+
+                pre = root.val;
+            }
+
+            find1(root.right);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
