03959ad530754649c05b70b66f5e8ff9f4b255b3
zhaozhifeng10 <444122886@qq.com>
2021-06-11 16:45:24 +0800

    tag:dp q:53 == 不会

--- src/main/java/learning/zhao/dp/Q53.java
+++ src/main/java/learning/zhao/dp/Q53.java
@@ -73,6 +73,20 @@ public class Q53 {
             return ret;
         }
     }
+
+   static class Solution2 {
+        public int maxSubArray(int[] nums) {
+            int[] dp = new int[nums.length];
+            dp[0] = nums[0];
+            int ret = dp[0];
+            for (int i = 1; i < nums.length; i++) {
+                dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+                ret = Math.max(dp[i], ret);
+            }
+
+            return ret;
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
