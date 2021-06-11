86f6aafe52aee7fef2e1057532462bdf23a7eda2
zhaozhifeng10 <444122886@qq.com>
2021-06-11 17:18:59 +0800

    tag:dp q:70 == 还是这题简单啊

--- src/main/java/learning/zhao/dp/Q70.java
+++ src/main/java/learning/zhao/dp/Q70.java
@@ -60,6 +60,29 @@ public class Q70 {
             return sum;
         }
     }
+
+    static class Solution2 {
+        public int climbStairs(int n) {
+            if (n == 1) {
+                return 1;
+            }
+
+            if (n == 2) {
+                return 2;
+            }
+
+            //dp[3] = dp[2] + dp[1]
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            dp[2] = 2;
+
+            for (int i = 3; i < dp.length; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[n];
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
