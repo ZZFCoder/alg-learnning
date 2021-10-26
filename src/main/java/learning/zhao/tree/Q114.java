0c2fef4f9dafe8e9bcff6ba05e1dad7c6ade574c
zhaozhifeng10 <zhaozhifeng10@jd.com>
2021-10-26 15:02:06 +0800

    tag:bitree --- easy

---
+++ src/main/java/learning/zhao/tree/Q114.java
@@ -0,0 +1,31 @@
+package learning.zhao.tree;
+
+import learning.zhao.material.TreeNode;
+
+/**
+ * @author zhaozhifeng10
+ * @program alg-learnning
+ * @description 114
+ * @since 2021/10/26 14:19
+ */
+public class Q114 {
+    static class Solution {
+        public void flatten(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+
+            flatten(root.left);
+            flatten(root.right);
+            TreeNode right1 = root.right;
+            root.right = root.left;
+            root.left = null;
+            TreeNode tmp = root;
+            while(tmp.right != null) {
+                tmp = tmp.right;
+            }
+
+            tmp.right = right1;
+        }
+    }
+}
