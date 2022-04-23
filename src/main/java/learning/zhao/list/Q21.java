eafd697c1a5e4f6e8b9caeeb2935001c6f88de21
zhaozhifeng10 <zhaozhifeng10@jd.com>
2022-04-23 19:48:45 +0800

    list:Q12 1.return xx.next 2.-> 转换的是副本

---
+++ src/main/java/learning/zhao/list/Q21.java
@@ -0,0 +1,35 @@
+package learning.zhao.list;
+
+public class Q21 {
+    static class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode ret = new ListNode();
+            ListNode tmp = ret;
+            while(list1 != null && list2 != null) {
+                if(list1.val <= list2.val) {
+                    tmp.next = list1;
+                    list1 = list1.next;
+                }
+                else {
+                    tmp.next = list2;
+                    list2 = list2.next;
+                }
+                tmp = tmp.next;
+            }
+
+            while(list1 != null) {
+                tmp.next = list1;
+                list1 = list1.next;
+                tmp = tmp.next;
+            }
+
+            while(list2 != null) {
+                tmp.next = list2;
+                list2 = list2.next;
+                tmp = tmp.next;
+            }
+
+            return ret.next;
+        }
+    }
+}
