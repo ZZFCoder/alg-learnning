0f6f814c268e41945b5fe1b71811c40c80440632
zhaozhifeng10 <zhaozhifeng10@jd.com>
2022-04-30 22:25:00 +0800

    tag:list Q21: 再来一遍

--- src/main/java/learning/zhao/list/Q21.java
+++ src/main/java/learning/zhao/list/Q21.java
@@ -32,4 +32,32 @@ public class Q21 {
             return ret.next;
         }
     }
+
+    static class Solution2 {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode ret = new ListNode();
+            ListNode tmp = ret;
+            while(list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    tmp.next = list1;
+                    list1 = list1.next;
+                }
+                else {
+                    tmp.next = list2;
+                    list2 = list2.next;
+                }
+                tmp = tmp.next;
+            }
+
+            if(list1 != null) {
+                tmp.next = list1;
+            }
+
+            if(list2 != null) {
+                tmp.next = list2;
+            }
+
+            return ret.next;
+        }
+    }
 }
