7a6c562e5f27d22140361da88ca2aac3be14f8e6
zhaozhifeng10 <zhaozhifeng10@jd.com>
2022-10-20 00:51:30 +0800

    再敲一遍. 这回一次过了.

---
+++ src/main/java/learning/zhao/tree/Q102_221020.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=java
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            int qz = queue.size();
+            List<Integer> row = new ArrayList<>();
+            for (int i = 0; i < qz; i++) {
+                TreeNode node = queue.poll();
+                row.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ret.add(row);
+        }
+
+        return ret;
+    }
+}
+// @lc code=end
+
