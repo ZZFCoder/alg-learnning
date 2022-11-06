23592e78c42779d630eebd29511715919b6d4bf3
zhaozhifeng10 <zhaozhifeng10@jd.com>
2022-11-06 13:08:59 +0800

    动态规划-开始

---
+++ src/main/java/learning/zhao/dp/Q300-221106.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=java
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int ret = 1;
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    ret = Math.max(ret, dp[i]);
+                }                
+            }
+        }
+
+        return ret;
+    }
+
+}
+// @lc code=end
+
