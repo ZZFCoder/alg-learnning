4d645ce629d184a86c5ff11ac59d833f04808369
zhaozhifeng10 <zhaozhifeng10@jd.com>
2022-12-10 21:40:41 +0800

    居然还记得.

---
+++ src/main/java/learning/zhao/tree/Q101-221210.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=java
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return is(root.left, root.right);
+    }
+
+    boolean is(TreeNode left, TreeNode right) {
+        if (left == right) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return is(left.left, right.right) && is(left.right, right.left);
+    }
+
+}
+// @lc code=end
+
