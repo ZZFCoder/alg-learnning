d02753dc87e3aeebc8417566fb34ccb4ccbe2015
ZZFCoder <444122886@qq.com>
2024-02-10 18:15:53 +0800

    Create Q300-240210.java
    又忘了

---
+++ src/main/java/learning/zhao/dp/Q300-240210.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=java
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int ret = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ret = Math.max(ret, dp[i]);
+        }
+        return ret;
+    }
+}
+// @lc code=end
+
